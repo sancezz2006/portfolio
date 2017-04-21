817e1ab87faaf87f18a0833c85ad83b4b5cd7f09
Alexander.M <sancezz2006@gmail.com>
2017-04-21 18:10:04 +0500

    Вёрстка index.pug

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,6 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('pug', function() {
+      
     return $.gulp.src('./source/template/pages/*.pug')
       .pipe($.gp.pug({ pretty: true }))
       .on('error', $.gp.notify.onError(function(error) {
@@ -10,6 +11,7 @@ module.exports = function() {
           message:  error.message
         }
        }))
+
       .pipe($.gulp.dest($.config.root));
   });
 };
